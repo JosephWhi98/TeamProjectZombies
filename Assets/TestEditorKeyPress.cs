05c6568294302b411993398992eb7fdaa9c83187
Andrew Nattress <a.nattress@gmail.com>
2021-03-24 13:14:46 +0000

    moved scripts and added test hotkey

---
+++ Assets/TestEditorKeyPress.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TestEditorKeyPress : MonoBehaviour
+{
+    public KeyCode key;
+    public UnityEvent e;
+    void Update()
+    {
+        if (Input.GetKeyDown(key))
+            e.Invoke();
+    }
+}
