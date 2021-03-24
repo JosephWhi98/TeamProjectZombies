1e5f788c64cef4d0322787fa5aeb5465c7e7f799
LiamMacalister <liam.macalister@outlook.com>
2021-03-24 14:15:12 +0000

    Movement and Shooting setup with Enemy health/Death
    Movement and Shooting setup with Enemy health/death first build

---
+++ Assets/_Scripts/ZombieScripts/Zombie.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Zombie : MonoBehaviour
+{
+    public float health = 50f;
+    
+    public void TakeDamage(float amntDamage)
+    {
+        health -= amntDamage;
+
+        if (health <= 0f)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
