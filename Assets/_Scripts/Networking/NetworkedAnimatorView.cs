40890fba0ea0f5b9a41dbbcabf77b425c21b4e36
Joseph Whitehead <joseph.whitehead101@gmail.com>
2021-03-31 15:20:23 +0100

    anim fix testing

--- Assets/_Scripts/Networking/NetworkedAnimatorView.cs
+++ Assets/_Scripts/Networking/NetworkedAnimatorView.cs
@@ -3,36 +3,20 @@ using Photon.Pun;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NetworkedAnimatorView : MonoBehaviourPun, IPunObservable
+
+public class NetworkedAnimatorView : MonoBehaviourPun
 {
     public Animator animator;
-
     public List<string> animatorTriggers;
 
-    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
-    {
-        if (stream.IsWriting)
-        {
-            foreach (string s in animatorTriggers)
-            {
-                stream.SendNext(animatorTriggers[0]);
-                animator.SetTrigger(s);
-            }
-
-            animatorTriggers.Clear();
-        }
-        else
-        {
-            string trigger = (string)stream.ReceiveNext();
-
-            if (!string.IsNullOrEmpty(trigger))
-                animator.SetTrigger(trigger);
-        }
-    }
-
-
+   
     public void TriggerAnimaton(string trigger)
     {
-        animatorTriggers.Add(trigger);
+        photonView.RPC("AnimateTrigger", RpcTarget.Others, trigger);
+    }
+
+    public void AnimateTrigger(string s)
+    {
+        animator.SetTrigger(s);
     }
 }
