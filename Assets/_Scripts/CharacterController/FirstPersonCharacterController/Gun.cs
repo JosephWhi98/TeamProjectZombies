f5f67a2256f298f1c5aa69bf38081befd920c081
LiamMacalister <w16021903@northumbria.ac.uk>
2021-04-05 14:12:41 +0100

    Gun system added and character model to scripts
    Created a shooting system (no sound or effects yet though) with a reloading system (currently debugging until UI is added). Cleaned up some of my files too.

--- Assets/_Scripts/CharacterController/FirstPersonCharacterController/Gun.cs
+++ Assets/_Scripts/CharacterController/FirstPersonCharacterController/Gun.cs
@@ -1,11 +1,25 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class Gun : MonoBehaviour
 {
     public float damage = 10f;
     public float range = 100f;
+    public float fireRate = 1;
+
+    public int totalAmmo = 10;
+    public int currentMag = 0;
+    public int maxMag = 10;
+    public float reloadTime = 1f;
 
     public Camera fpsCam;
+
+    private bool isReloading = false;
+
+    void Awake()
+    {
+        StartCoroutine(Reload());
+    }
 
     // Update is called once per frame
     void Update()
@@ -14,21 +28,62 @@ public class Gun : MonoBehaviour
         {
             Shoot();
         }
+
+        if (Input.GetButtonDown("Reload") && isReloading == false)
+        {
+            StartCoroutine(Reload());
+        }
     }
 
     void Shoot()
     {
         RaycastHit hit;
 
-        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        if (currentMag > 0)
         {
-            Debug.Log(hit.transform.name);
-
-            Zombie zombie = hit.transform.GetComponent<Zombie>();
-            if (zombie != null)
+            currentMag -= 1;
+            if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
             {
-                zombie.TakeDamage(damage);
+                Zombie zombie = hit.transform.GetComponent<Zombie>();
+                if (zombie != null)
+                {
+                    zombie.TakeDamage(damage);
+                }
             }
+
+            Debug.Log("Current: " + currentMag + "  " + "Total Ammo: " + totalAmmo);
+
         }
+
+        else
+        {
+            Reload();
+        }
+    }
+
+    IEnumerator Reload()
+    {
+        isReloading = true;
+        yield return new WaitForSeconds(reloadTime);
+
+        if (totalAmmo <= 0)
+        {
+            yield break;
+        }
+
+        int ammoToLoad = maxMag - currentMag;
+        if (ammoToLoad > totalAmmo)
+        {
+            currentMag += totalAmmo;
+            totalAmmo = 0;
+        }
+
+        else
+        {
+            currentMag = maxMag;
+            totalAmmo -= ammoToLoad;
+        }
+
+        isReloading = false;
     }
 }
