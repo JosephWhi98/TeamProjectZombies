051bc2b5a14c115f753fef2d275ee63190a27931
WillertonJR <34305299+WillertonJR@users.noreply.github.com>
2021-04-25 20:34:50 +0100

    Add files via upload

--- Assets/_Scripts/AI/AIManager.cs
+++ Assets/_Scripts/AI/AIManager.cs
@@ -11,6 +11,12 @@ public class AIManager : MonoBehaviour
     private IAISpawnHandler aiSpawner;
     [Header("DebugInfo")]
     public List<AIBase> baseAIComponents;
+
+    public int waveNumber = 0;
+    public int enemySpawnAmount = 0;
+    public int enemiesKilled = 0;
+
+
     public void Awake()
     {
         baseAIComponents = new List<AIBase>();
@@ -19,6 +25,8 @@ public class AIManager : MonoBehaviour
     private void Start()
     {
         aiSpawner = GetComponent<IAISpawnHandler>();
+
+        StartWave();
     }
 
     public void SpawnNewAI(AIType aiType)
@@ -51,6 +59,44 @@ public class AIManager : MonoBehaviour
         //TODO - window target needs to be determined based on how many enemies are already targeting a window. 
         int index = Random.Range(0, BaseScene.Instance.enterancePortals.Length);
         return BaseScene.Instance.enterancePortals[index].entranceTarget;
+    }
+
+    public void StartWave()
+    {
+        waveNumber = 1;
+        enemySpawnAmount = 2;
+        enemiesKilled = 0;
+        for (int i = 0; i < enemySpawnAmount; i++)
+        {
+            SpawnNewAI(AIType.ZOMBIE);
+        }
+    }
+
+    public void NextWave()
+    {
+        waveNumber++;
+        enemySpawnAmount += 3;
+        enemiesKilled = 0;
+        for (int i = 0; i < enemySpawnAmount; i++)
+        {
+            SpawnNewAI(AIType.ZOMBIE);
+        }
+    }
+
+    public void Update()
+    {
+        if (enemiesKilled >= enemySpawnAmount)
+        {
+            StartCoroutine(Wait());
+        }
+    }
+
+    private IEnumerator Wait()
+    {
+        enemiesKilled = 0;
+        yield return new WaitForSeconds(15);
+
+        NextWave();
     }
 
 #if UNITY_EDITOR
